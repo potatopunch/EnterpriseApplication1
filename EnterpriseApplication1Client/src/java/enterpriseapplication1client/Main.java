a6018576efef965f8dc4375f08f9d162dccc9b8a
potatopunch <ilovepiano92@hotmail.com>
2018-10-03 08:52:46 +0800

    modified client main

--- EnterpriseApplication1Client/src/java/enterpriseapplication1client/Main.java
+++ EnterpriseApplication1Client/src/java/enterpriseapplication1client/Main.java
@@ -5,7 +5,8 @@ package enterpriseapplication1client;
 public class Main 
 {
     public static void main(String[] args) 
-    {        
+    {
+        System.out.println("Hello World!");
     }
     
 }
